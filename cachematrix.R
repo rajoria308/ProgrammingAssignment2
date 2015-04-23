302b94f131b3b4ffb3761fa4c69adb0700e28b28
Nitish Rajoria <rajorianitish308@gmail.com>
2015-04-24 00:52:10 +0530

    Assignment 2 of 'Inverse Matrix' uploaded with modifications

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,37 @@
+##Script to evaluate the inverse matrix as well as cache the inverse matrix
+##so that it can be retrived as required if no changes are made to the supplied
+##matrix
 
 makeCacheMatrix <- function(x){
         matVal <- NULL
-        setVal <- function(y){
+        
+        setVal <- function(y){   #This function assigns new matrix to 'x'     
                 x <<- y
-                matVal <<- NULL
+                
+                matVal <<- NULL  #The value of should be reverted back to NULL
         }
-        getVal <- function() x
-        setMat <- function(matSolve) matVal <<- matSolve
-        getMat <- function() matVal
+        getVal <- function() x   #Returns the matrix set by setVal
+        
+        setMat <- function(matSolve) matVal <<- matSolve     #Sets the inverse matrix    
+        
+        getMat <- function() matVal     #Returns the desired inverse matrix      
+        
+        #Formation of list that contains functions 
         list(set = setVal, get = getVal, setSolved = setMat, getSolved = getMat)
 }
 
+#Inverse matrix to be supplied by this function below
 cacheSolve <- function(x, ...){
-        matVal <- x$getSolved()
-        if(!is.null(matVal)){
+        matVal <- x$getSolved()         #Sets the matVal with previous inverse matrix
+        
+        if(!is.null(matVal)){           #Condition for checking if previous result was NULL or not
                 message("Getting cached data")
-                return (matVal)
+                
+                return (matVal)         #if not then returns the cached inverse matrix
         }
-        dataMat <- x$get()
-        matVal <- solve(dataMat)
-        x$setSolved(matVal)
+        dataMat <- x$get()              #if yes then sets the dataMat variable with new matrix
+        
+        matVal <- solve(dataMat)        #Special function 'Solve' to retrieve inverse matrix of supplied matrix
+        
+        x$setSolved(matVal)             #Sets the function with a inverse matrix 
 }
